fce7b354cc0555db3d1ea195ff53e21bb7126086
Michael Hart <michaeljh619@yahoo.com>
2018-11-15 11:31:19 -0800

    feat: add basic python flask file
    MINOR: update gitignore to ignore .swp extension

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# imports
+from flask import Flask, render_template, request
+from flask import redirect, url_for, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# flask app
+app = Flask(__name__)
+
+# test page
+@app.route('/')
+def test():
+    return "<html><body>Hello there!</body></html>"
+
+# if main
+if __name__ == '__main__':
+    # allows code update w/o server restart
+    app.debug = True
+    # listen on all addresses
+    app.run(host='0.0.0.0', port=5000)
