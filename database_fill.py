d4bac606174d0c3ae58680058f0b7236ebdc8aa7
Michael Hart <michaeljh619@yahoo.com>
2018-11-17 06:48:11 -0800

    feat: add database and update back-end for sql
    Some samples added in database and so far the pages that
    display stories and categories will work.
    chore: add new, edit, delete stories/pages

---
+++ database_fill.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+# sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+# database tables
+from database_setup import Base, Category, Story, Story_Page, Page_Link
+
+# set up session
+engine = create_engine('sqlite:///stories.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# clear database
+session.query(Category).delete()
+session.query(Story).delete()
+session.query(Story_Page).delete()
+session.commit()
+
+# create categories
+categories = [ Category(name="Action"),
+               Category(name="Adventure"),
+               Category(name="Horror") ]
+for c in categories:
+    session.add(c)
+    session.commit()
+
+# create stories
+story1 = Story(name="Action Jack",
+               description="Explosions, car chases, and action!",
+               category=categories[0])
+story2 = Story(name="Action Phil",
+               description="Action Jack's brother, not as cool.",
+               category=categories[0])
+story3 = Story(name="The Mage's Path",
+               description="Wield magic in a medieval realm!",
+               category=categories[1])
+story4 = Story(name="Lost in the Woods",
+               description="You are lost in a forest, but not alone...",
+               category=categories[2])
+session.add(story1)
+session.add(story2)
+session.add(story3)
+session.add(story4)
+
+# create story pages
+story1_page1 = Story_Page(name="Start Story",
+                          description="Action Jack in a car chase",
+                          text="Action Jack is being chased in a car!",
+                          story=story1,
+                          is_root=True)
+story1_page2a = Story_Page(name="Hit the brakes",
+                          description="Action Jack hits the brakes", 
+                          text="He hits the brakes and the baddies zoom by",
+                          story=story1,
+                          is_root=False)
+story1_page2b = Story_Page(name="Hit the gas",
+                          description="Action Jack slams the gas", 
+                          text="He floors it and gets away!",
+                          story=story1,
+                          is_root=False)
+session.add(story1_page1)
+session.add(story1_page2a)
+session.add(story1_page2b)
+session.commit()
+
+# create page links
+page1_link_a = Page_Link(base_page_id=story1_page1.id,
+                         linked_page_id=story1_page2a.id)
+page1_link_b = Page_Link(base_page_id=story1_page1.id,
+                         linked_page_id=story1_page2b.id)
+session.add(page1_link_a)
+session.add(page1_link_b)
+session.commit()
+
+# close database
+session.close()
--- main.py
+++ main.py
@@ -5,34 +5,98 @@ from flask import Flask, render_template, request
 from flask import redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Story, Story_Page, Page_Link
 
 # flask app
 app = Flask(__name__)
+
+
+# sql session creation
+def create_session():
+    engine = create_engine('sqlite:///stories.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    return DBSession()
 
 
 # main page showing all categories
 @app.route('/')
 @app.route('/categories/')
 def showCategories():
-    return render_template("categories.html")
+    # get categories from database
+    session = create_session()
+    categories = session.query(Category).all()
+    # close database session
+    session.close()
+    return render_template("categories.html", categories=categories)
 
 
 # show all stories under a category
-@app.route('/categories/<string:category>/')
-def showCategory(category):
+@app.route('/categories/<int:category_id>/')
+def showCategory(category_id):
+    # start sql session
+    session = create_session()
+    # get category and check it's valid
+    category = session.query(Category).get(category_id)
+    if not category:
+        return None
+    # get stories
+    stories = session.query(Story).filter_by(category_id=category.id)
+    # close database session
+    session.close()
     return render_template("showCategory.html",
-                           category=category)
+                           category=category,
+                           stories=stories)
 
 
 # show a particular story in a category
-@app.route('/categories/<string:category>/story/<int:story_id>')
-def showStory(category, story_id):
-    return render_template("showStory.html")
+@app.route('/categories/<int:category_id>/story/<int:story_id>/page/<int:page_id>')
+def showStory(category_id, story_id, page_id):
+    # start sql session
+    session = create_session()
+    # get category
+    category = session.query(Story).get(category_id)
+    if not category:
+        return None
+    # get story
+    story = session.query(Story).get(story_id)
+    print(str(story_id))
+    if not story:
+        return None
+    # get root page if a page is not specified
+    page = None
+    print(str(page_id))
+    if page_id == 0:
+        page = session.query(
+            Story_Page).filter_by(is_root=True,
+                                  story_id=story.id).one()
+    # page was specified
+    else:
+        page = session.query(Story_Page).get(page_id)
+    # check page is valid
+    if not page:
+        return None
+    # get linked pages
+    page_links = session.query(Page_Link).filter_by(
+                    base_page_id=page.id).all()
+    linked_pages = []
+    for page_link in page_links:
+        linked_page = session.query(Story_Page).get(
+                        page_link.linked_page_id)
+        linked_pages.append(linked_page)
+        print(linked_page.name)
+    # close sql session
+    session.close()
+    return render_template("showStory.html",
+                           category=category,
+                           story=story,
+                           page=page,
+                           linked_pages=linked_pages)
 
 
 # new story page
-@app.route('/categories/<string:category>/story/new')
-def newStory(cat_id):
+@app.route('/categories/<int:category_id>/story/new')
+def newStory(category_id):
     return render_template("newStory.html")
 
 
