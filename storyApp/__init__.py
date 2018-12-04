a245d2b9845909db0085d798f3c0a6be266a9807
Michael Hart <michaeljh619@yahoo.com>
2018-12-04 08:54:39 -0800

    refactor: use function in init to create app
    create_app function in the main __init__ file is now used to actually
    return a created app with parameters

--- storyApp/__init__.py
+++ storyApp/__init__.py
@@ -21,3 +21,27 @@ import storyApp.route_editPages
 import storyApp.route_addPage
 import storyApp.route_editPage
 import storyApp.route_deletePage
+
+# function to create app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    # test config
+    if test_config is None:
+        # load instance config, if exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
