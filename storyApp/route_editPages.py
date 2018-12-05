adbe32b217da4af8c67247752788a13b48a3795c
Michael Hart <michaeljh619@yahoo.com>
2018-12-05 07:58:36 -0800

    feat: add collapsing tree list to page edit
    - user can now collapse and expand a tree list showing the path of story pages in the "edit_pages" section of a story.
    chore: need to seperate javascript from HTML and "pretty" the page.

--- storyApp/route_editPages.py
+++ storyApp/route_editPages.py
@@ -14,6 +14,36 @@ def create_session():
     DBSession = sessionmaker(bind=engine)
     return DBSession()
 
+def create_page_tree(page, parent, depth):
+    # get all page links
+    session = create_session()
+    page_links = session.query(Page_Link).filter_by(
+                base_page_id=page.id).all()
+    # get all linked pages
+    linked_pages = []
+    linked_pages_trees = []
+    # in a branch (has linked pages)
+    if len(page_links) > 0:
+        # create array of pages
+        for page_link in page_links:
+            # query linked page
+            linked_page = session.query(Story_Page).get(
+                            page_link.linked_page_id)
+            linked_pages.append(linked_page)
+        # close session
+        session.close()
+        # create array of page trees
+        for linked_page in linked_pages:
+            linked_page_tree = create_page_tree(linked_page,
+                                                page, depth+1)
+            linked_pages_trees.append(linked_page_tree)
+    # in a leaf (no linked pages)
+    else:
+        session.close()
+    # return page tree
+    return (page, linked_pages_trees, parent, depth)
+    
+
 # pick a page to add a linked page to
 @app.route('/categories/<int:category_id>/story/'
            + '<int:story_id>/pages')
@@ -36,7 +66,15 @@ def editPages(category_id, story_id):
                                 story_id=story_id,
                                 linking_page_id=0))
     else:
+        # get root page
+        session = create_session()
+        root_page = session.query(Story_Page).filter_by(
+                story_id=story.id,
+                is_root=True).one()
+        session.close()
+        # create page tree
+        page_tree = [create_page_tree(root_page, None, 0)]
         return render_template("editPages.html",
                                story=story,
                                category=category,
-                               pages=pages)
+                               page_tree=page_tree)
